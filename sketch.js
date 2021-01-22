cb4c4101cf92b626387c8b6c51539ab6a4765133
Swapnilbhattacharya <swapnilmpbfhs@gmail.com>
2021-01-22 18:14:30 +0530

    constrainedLog

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
+var constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,12 +18,16 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
+    constrainedLog=new Log(230,180,80,PI/2);
+
+    ground = new Ground(600,height,1200,20);
+
+    platform=new Ground(150,300,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
+ 
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
@@ -32,20 +39,23 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230,180,80,PI/2);
+    
+    chain1= new Chain(bird.body,log6.body);
 
     bird = new Bird(100,100);
-
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
     console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
+    console.log(box2.body.position.y);  
     console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
+    platform.display();
     pig1.display();
     log1.display();
 
@@ -53,10 +63,16 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
+    constrainedLog.display();
 
     box5.display();
     log4.display();
     log5.display();
+    strokeWeight(4);
+    line(bird.body.position.x,bird.body.position.y,
+        constrainedLog.body.position.x, constrainedLog.body.position.y);
 
     bird.display();
+    log6.display();
+    chain1.display();
 }
